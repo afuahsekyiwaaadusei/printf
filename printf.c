7637b630c35aee4649a4f81c9f8e729cf0e93e79
precious-appiah <appiahprecious99@gmail.com>
2023-05-30 13:44:51 -0700

    bug fixes

--- printf.c
+++ printf.c
@@ -1,46 +1,51 @@
 #include "main.h"
 #include <unistd.h>
 #include <stdarg.h>
-
 /**
  *_printf - function that  produces output according to a format.
  *@format: The format string is composed of zero or more directives.
  *
  *Return:integer(number of characters printed excluding \0)
  */
-
 int _printf(const char *format, ...)
 {
 	va_list arg;
-	int count = 0, i = 0;
+	int count = 0, i = 0, j;
 	char ch;
+	char *str;
 
 	va_start(arg, format);
-
 	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
-			while (i <= 6)
+			i = 1;
+			while (i <= 7)
 			{
-				if (*format == 'c')
+				if (format[i] == 'c')
 				{
 					ch = va_arg(arg, int);
 					write(1, &ch, 1);
 					i = 7;
 					count++;
-					format++;
-
+					format += 2;
 				}
-
+				if (format[i] == 's')
+				{
+					str = va_arg(arg, char *);
+					for (j = 0; str[j] != '\0'; j++)
+					{
+						write(1, str + j, 1);
+						count += j;
+					}
+					format += 2;
+				}
+				i++;
 			}
 		}
-
 		write(1, format, 1);
 		count++;
 		format++;
 	}
-
 	return (count);
-
 }
