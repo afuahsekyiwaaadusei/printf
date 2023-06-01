1ad5309a309bcd685f5595af3481ff7c616a04ac
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-06-01 08:48:32 -0700

    fixes

--- printf.c
+++ printf.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <unistd.h>
 #include <stdarg.h>
-
 /**
  *_printf - function that  produces output according to a format.
  *@format: The format string is composed of zero or more directives.
@@ -11,50 +10,65 @@
 int _printf(const char *format, ...)
 {
 	va_list arg;
-	int count = 0, i = 0, j;
+	int count = 0, j;
 	char ch;
 	char *str;
 
 	va_start(arg, format);
-	while (*format != '\0')
+	if (format == NULL)
 	{
-		if (*format == '%')
+		count = -1;
+	}
+	else
+	{
+		while (*format != '\0')
 		{
-			format++;
-			while (i < 7)
+			if (*format != '%')
 			{
-				if (format[i] == 'c')
+				write(1, format, 1);
+				count++;
+				format++;
+			}
+			else if (*format == '%' && *(format + 1) == 'c')
+			{
+				ch = va_arg(arg, int);
+				write(1, &ch, 1);
+				count++;
+				format += 2;
+			}
+			else if (*format == '%' && *(format + 1) == 's')
+			{
+				str = va_arg(arg, char *);
+				if (str == NULL)
 				{
-					ch = va_arg(arg, int);
-					write(1, &ch, 1);
-					i = 7;
-					count++;
-					format++;
+					write(1,"(null)", 6);
+					count += 6;
 				}
-				if (format[i] == 's')
+				else
 				{
-					str = va_arg(arg, char *);
 					for (j = 0; str[j] != '\0'; j++)
 					{
 						write(1, str + j, 1);
-						count += j;
+						count++;
 					}
-					i = 7;
-					format++;
 				}
-				if (format[i] == '%')
-				{
-					write(1, format, 1);
-					i = 7;
-					count++;
-					format++;
-				}
-				i++;
+				format += 2;
 			}
+			else if (*format == '%' && *(format + 1) == '%')
+			{
+				write(1, format, 1);
+				count++;
+				format += 2;
+			}
+			else if (*format == '%')
+			{
+				write(1, format, 1);
+				count++;
+				format++;
+			}
+			else
+				return (-1);
 		}
-		write(1, format, 1);
-		count++;
-		format++;
 	}
 	return (count);
 }
